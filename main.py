eb0cc9d08bbe57fedd016dbcbeda10426217d9a1
CrazyAlan <xiaochuan.chen91@gmail.com>
2016-01-25 23:09:45 -0800

    finished downsampling

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ import ImageTransform as imt
 import dct 
 
 
-quantMat = np.full([8,8], 100)
+quantMat = np.full([8,8], 1)
 
 
 orgImage = imt.ImageTransform()
 orgRGB = orgImage.readImage("kodim23.png")
-print np.shape(orgRGB)
 
-F_info, orgShape = coding.encode(orgRGB, quantMat)
+
+imgPIL = orgImage.rgb2img(orgRGB)
+halfImg = imgPIL.resize((np.shape(orgRGB)[1]/2, np.shape(orgRGB)[0]/2))
+quartImg = imgPIL.resize((np.shape(orgRGB)[1]/4, np.shape(orgRGB)[0]/4))
+#tmp.show()
+recImg = quartImg.resize((np.shape(orgRGB)[1], np.shape(orgRGB)[0]))
+recImg.show()
+F_info, orgShape = coding.encode(np.array(quartImg), quantMat)
 
 coding.decode(F_info, quantMat, orgShape)
