4088fb10b9a08396efbc18f195cfbf6bd18611e4
CrazyAlan <xiaochuan.chen91@gmail.com>
2016-01-26 12:00:54 -0800

    seems right hier encoding

--- hierarchical.py
+++ hierarchical.py
@@ -1,11 +1,48 @@
 import numpy as np
+from PIL import Image
+import numpy as np
+import ImageTransform as imt
+import coding 
+
+
 
 
 class Hier():
 	"""docstring for Hier"""
-	def __init__(self):
+	def __init__(self, quantMat):
 		self.data = []
+		self.IMT = imt.ImageTransform()
+		self.quantMat = quantMat
 
-	def reduceResolution(f, ratio):
-		
+	def hierEncode(self, f_half, f):
+		highResShape = np.shape(f)
+		E_f_half = imt.ImageTransform.imresize(self.IMT.rgb2img(f_half), highResShape, 1)
+		E_f_half_RGB = np.array(E_f_half)
+
+		diffRGB = f - E_f_half_RGB
+		F_info, orgShape = coding.encode(np.array(diffRGB), self.quantMat)
+		recRGB, recImg2show = coding.decode(F_info, self.quantMat, orgShape)
+
+		return F_info, orgShape, recRGB, recImg2show
+
+	def hireDecode(self, F_info_half, F_info_diff, diffShape):	
+		orgHalfShape = (np.array(diffShape[0])/2, np.array(diffShape[1])/2, 3)
+		print orgHalfShape
+		recRGBHalf, recImg2showHalf = coding.decode(F_info_half, self.quantMat, orgHalfShape)
+
+		#recImg2showHalf.show()
+		#diffShape = (np.shape(F_info_diff[0])[0], np.shape(F_info_diff[0])[1], 3)
+		print diffShape
+		recRGBDiff, recImg2showDiff = coding.decode(F_info_diff, self.quantMat, diffShape)
+
+		#add up
+		E_f_half = imt.ImageTransform.imresize(self.IMT.rgb2img(recRGBHalf), diffShape, 1)
+		E_f_half_RGB = np.array(E_f_half)
+
+		fRecover = recRGBDiff + E_f_half_RGB
+		fRecoverImg = self.IMT.rgb2img(fRecover)
+
+		return fRecover, fRecoverImg
+
+
 		
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import scipy as sp
 import quantization as quant
 import ImageTransform as imt
 import dct 
+import hierarchical as hr
 
 
 quantMat = np.full([8,8], 1)
@@ -20,11 +21,22 @@ quartImg = imt.ImageTransform.imresize(imgPIL, np.shape(orgRGB), 4)
 '''Hierachical Encoding'''
 #Encode quartImg
 F_info_quart, orgShape_quart = coding.encode(np.array(quartImg), quantMat)
+#print orgShape_quart
 #Decode QuartImg
 recRGB_quart, recImg2show_quart = coding.decode(F_info_quart, quantMat, orgShape_quart)
 #recImg2show_quart.show()
 
-#Encode half  Image
+#Encode half diff  Image
+hierVar = hr.Hier(quantMat)
+halfdiffF_info, halfdifforgShape, hafldiffrecRGB, halfdiffrecImg2show = hierVar.hierEncode(np.array(quartImg), np.array(halfImg))
+#Encode whole diff Image
+diffF_info, difforgShape, diffrecRGB, diffrecImg2show = hierVar.hierEncode(np.array(halfImg), np.array(imgPIL))
+
+
+fRecoverHalf, fRecoverHalfImg = hierVar.hireDecode(F_info_quart, halfdiffF_info, halfdifforgShape)
+fRecover, fRecoverImg = hierVar.hireDecode(fRecoverHalf, diffF_info, difforgShape)
+
+#fRecoverHalfImg.show()
 
 
 
