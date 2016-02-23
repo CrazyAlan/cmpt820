6f43b77b73ea72c8b3e1103da5f52b9ec4302688
albertdream <albertdream1991@gmail.com>
2016-02-22 21:23:49 -0800

    finished intra coding

--- Intra4by4Coding.py
+++ Intra4by4Coding.py
@@ -1,12 +1,12 @@
 import numpy as np
-from IntegerTransform improt *
+from IntegerTransform import *
 class IntraCoding():
 	"""This class is for intra coding and decoding for I frames"""
-	def __init__(self):
+	def __init__(self,QP):
 		#self.deintra_image = []
 		self.intra_image = []
-		IT = IntegerTransform()
-
+		self.IT = IntegerTransform()
+		self.IT.QuantizationMatrix(QP)
 		
 
 	def IntraEncoding(self,Y,Y_r,i,j,N):
@@ -14,13 +14,14 @@ class IntraCoding():
 		cols = j - 1
 		T = Y_r[rows,cols+1:cols+N+1]
 		L = Y_r[rows+1:rows+N+1,cols]
-		c0 = mode0(Y,T,N)
-		c1 = mode0(Y,L,N)
-		c2 = mode0(Y,L,T,N)
+		c0 = self.mode0(Y,T,N)
+		c1 = self.mode1(Y,L,N)
+		c2 = self.mode2(Y,L,T,N)
 		BK = Y[rows+1:rows+1+N,cols+1:cols+1+N]
-		SOD(0) = sum(sum(abs(c0-BK)))
-		SOD(1) = sum(sum(abs(c1-BK)))
-		SOD(2) = sum(sum(abs(c2-BK)))
+		SOD = [0,0,0]
+		SOD[0] = sum(sum(abs(c0-BK)))
+		SOD[1] = sum(sum(abs(c1-BK)))
+		SOD[2] = sum(sum(abs(c2-BK)))
 		mode = SOD.index(min(SOD))
 		if (mode == 0):
 			self.intra_image[i:i+4,j:j+4] = c0
@@ -51,38 +52,38 @@ class IntraCoding():
 	def IntraCodingVec(self,Y):
 		self.intra_image = np.zeros_like(Y)
 		Y_r = np.zeros_like(Y)
-		self.mode = np.zeros((np.shape(Y)[0]//4,np.shape(Y)[1]//4), dtype=np.int)
 		for m in range(np.shape(Y)[0]//16):
 			for n in range(np.shape(Y)[1]//16):
 				for i in range(m*4,m*4+4):
 					for j in range(n*4,n*4+4):
-						BK = Y[i:i+N,j:j+N]
-						if (i == 0 && j == 0):
-							mode = 3;
-							ICP_en = IT.EnIntegerTransform(BK)
-							Y_r = IT.DeIntegerTransform(ICP_en)
+						BK = Y[i*4:i*4+4,j*4:j*4+4]
+						if (i == 0 and j == 0):
+							mode = 3
+							ICP_en = self.IT.EnIntegerTransform(BK)
+							Y_r[i*4:i*4+4,j*4:j*4+4] = self.IT.DeIntegerTransform(ICP_en)
 						elif (j == 0):
 							rows = i - 1
-							T = Y_r[rows,cols+1:cols+k+1]
-							mode = 0;
-							self.intra_image[i*4:i*4+4,j*4:j*4+4] = mode0(Y,T,4)
+							T = Y_r[rows,0:4]
+							mode = 0
+							self.intra_image[i*4:i*4+4,j*4:j*4+4] = self.mode0(Y,T,4)
 							ICP = BK - self.intra_image[i*4:i*4+4,j*4:j*4+4]
-							ICP_en = IT.EnIntegerTransform(ICP)
-							ICP_r = IT.DeIntegerTransform(ICP_en)
+							ICP_en = self.IT.EnIntegerTransform(ICP)
+							ICP_r = self.IT.DeIntegerTransform(ICP_en)
 							Y_r[i*4:i*4+4,j*4:j*4+4] = self.intra_image[i*4:i*4+4,j*4:j*4+4] + ICP_r
 						elif (i == 0):
 							cols = j - 1
-							L = Y_r[rows+1:rows+k+1,cols]
-							mode = 1;
-							self.intra_image[i*4:i*4+4,j*4:j*4+4] = mode1(Y,L,4)
+							L = Y_r[0:4,cols]
+							mode = 1
+							self.intra_image[i*4:i*4+4,j*4:j*4+4] = self.mode1(Y,L,4)
 							ICP = BK - self.intra_image[i*4:i*4+4,j*4:j*4+4]
-							ICP_en = IT.EnIntegerTransform(ICP)
-							ICP_r = IT.DeIntegerTransform(ICP_en)
+							ICP_en = self.IT.EnIntegerTransform(ICP)
+							ICP_r = self.IT.DeIntegerTransform(ICP_en)
+	
 							Y_r[i*4:i*4+4,j*4:j*4+4] = self.intra_image[i*4:i*4+4,j*4:j*4+4] + ICP_r
 						else:
-							ICP = self.IntraCoding(Y,Y_r,i*4,j*4,4)
-							ICP_en = IT.EnIntegerTransform(ICP)
-							ICP_r = IT.DeIntegerTransform(ICP_en)
+							ICP = self.IntraEncoding(Y,Y_r,i*4,j*4,4)
+							ICP_en = self.IT.EnIntegerTransform(ICP)
+							ICP_r = self.IT.DeIntegerTransform(ICP_en)
 							Y_r[i*4:i*4+4,j*4:j*4+4] = self.intra_image[i*4:i*4+4,j*4:j*4+4] + ICP_r
 
 		return Y_r
@@ -115,6 +116,12 @@ class IntraCoding():
 		out = np.zeros_like(Y[0:4,0:4])
 		for i in range(N):
 			for j in range(N):
-				out[i,j] = round(np.mean(np.concatenate(np.concatenate(L[0:N],T[0:N]),np.array([4]))))
+				out[i,j] = round(np.mean(np.concatenate([L,T]))+4/9.0)
 		return out
+
+if __name__ == '__main__':
+	Y = np.load("Y.npy")
+	IC = IntraCoding(30)
+	Y_r = IC.IntraCodingVec(Y)
+	np.save("Y_r.npy",Y_r)
 		
